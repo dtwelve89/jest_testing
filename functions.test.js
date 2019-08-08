d6a3c5875fc637f73542970f8e7611e62c5052b2
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-08-08 14:47:40 -0700

    Initial commit

---
+++ functions.test.js
@@ -0,0 +1,36 @@
+const functions = require('./functions');
+
+// toBe
+test('Adds 2 + 2 to equal 4', () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+// not
+test('Adds 2 + 2 to NOT equal 5', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// toBeNull
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// tobeFalsy
+test('Should be falsy', () => {
+  expect(functions.checkValue(undefined)).toBeFalsy();
+});
+
+// toEqual
+test('User should be Danny Nguyen object', () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Danny',
+    lastName: 'Nguyen'
+  });
+});
