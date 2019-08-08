ebfcae4731108d6391b3f402d8245a2ee8f3768a
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-08-08 16:52:52 -0700

    Jest testing basics

--- functions.test.js
+++ functions.test.js
@@ -34,3 +34,45 @@ test('User should be Danny Nguyen object', () => {
     lastName: 'Nguyen'
   });
 });
+
+// Lesser/greater than
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
+// Lesser/greater than or equal
+test('Should be equal to 1600', () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/i);
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+  usernames = ['joe', 'karen', 'admin'];
+  expect(usernames).toContain('admin');
+});
+
+// Working with async data
+
+// Promise
+// test('User fetched name should be Leanne Graham', () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then(data => {
+//     expect(data.name).toEqual('Leanne Graham');
+//   });
+// });
+
+// Async await
+test('User fetched name should be Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
+});
